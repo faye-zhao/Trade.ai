8cfb296cb62d61f8313f1c2cf5b4dad720cfa7ac
faye zhao <fzhire@gmail.com>
2024-12-25 12:07:34 -0800

    update

---
+++ Trade.ai/SettingsView.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct SettingsView: View {
+    var body: some View {
+        
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Settings")
+        }
+        .padding()
+        
+        WatchCardView()
+    }
+}
